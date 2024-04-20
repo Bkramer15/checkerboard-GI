3111d1a7589aaf9b097e75b72e9b7a1f2d79a807
Birhanu C kramer <Birhanuckramer@AdminisatorsMBP.home>
2024-04-19 21:43:56 -0400

    repo

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('button').addEventListener('click',redirect);
+  });
+    function redirect(){
+      window.location.href="index.html";
+    }
+  
